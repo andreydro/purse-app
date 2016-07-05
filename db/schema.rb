b4ee119f61af41f015c055fbad79af4221ef1a77
dro14 <and-rey-2309@mail.ru>
2016-07-05 20:54:44 +0300

    Add users and static_pages controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160705173421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "static_pages", force: :cascade do |t|
+    t.string   "static_pages"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "users"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
